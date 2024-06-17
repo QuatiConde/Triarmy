58393803317e7fb46150f2890d74f197a4bda445
QuatiConde <victormanoelxzk@hotmail.com>
2024-06-16 21:50:04 -0300

    v 0 2

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-TriArmy-0.1.9";
+const cacheName = "DefaultCompany-TriArmy-0.2";
 const contentToCache = [
     "Build/Triarmy.loader.js",
     "Build/Triarmy.framework.js",
